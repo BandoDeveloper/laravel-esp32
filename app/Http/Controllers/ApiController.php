b22361864bf10aee5ea9e67413825d063be8b90b
Esteban Medrano Chacolla <bandodevelopment@gmail.com>
2024-08-19 23:23:58 -0400

    firstApi

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,7 +16,15 @@ class ApiController extends Controller
     function obtenerEstadoLed(){
         $sql = DB::select("SELECT obtener_ultimo_estado();");
         $cond = get_object_vars($sql[0])['obtener_ultimo_estado'];
-        return response(get_object_vars($sql[0])['obtener_ultimo_estado'], 200);
+        if ($cond == 'true') {
+            return response("encendido", 200);
+        }
+        else if($cond == 'false'){
+            return response("apagado", 200);
+        }
+        else{
+            return response("Error", 404);
+        }
         /*if($sql["estado"] == 1){
             echo "encendido";
         }
