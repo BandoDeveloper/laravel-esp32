b7dc56fbf327fb2fd30cd2d9ebec04d72fe6fe83
BandoDeveloper <bandodeveloper@gmail.com>
2024-08-20 08:33:20 -0400

    DeployApi

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,5 @@ Route::get('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('ve
 Route::post('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
 Route::get('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
 Route::post('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
-Route::get('/toggleLed', [ApiController::class,'toggleLed'])->name('toggleLed');
-Route::post('/toggleLed', [ApiController::class,'toggleLed'])->name('toggleLed');
+Route::get('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
+Route::post('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
