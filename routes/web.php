b43367227b5a5c7a796908bd31fabb22a363178b
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-05 23:49:54 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-Route::get('/', [ApiController::class,'EspLocation'])->name('A9gLocation');
+Route::get('/', [ApiController::class,'A9gLocation'])->name('A9gLocation');
 //Route::get('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
 //Route::post('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
 //Route::get('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
 //Route::post('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
 //Route::get('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
 //Route::post('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
-Route::get('/A9gLocation', [ApiController::class,'EspLocation'])->name('A9gLocation');
+Route::get('/A9gLocation', [ApiController::class,'A9gLocation'])->name('A9gLocation');
 Route::post('/LocationFromDB', [ApiController::class,'LocationFromDB'])->name('LocationFromDB');
 Route::get('/LocationFromDB', [ApiController::class,'LocationFromDB'])->name('LocationFromDB');
 //ENVIAR DATOS DEL DHT
