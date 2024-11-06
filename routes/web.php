9ca753146c52b3e648e4a9958deccbdb6fc72846
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-05 23:36:05 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\ApiController;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
-Route::post('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
-Route::get('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
-Route::post('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
-Route::get('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
-Route::post('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
-Route::get('/EspLocation', [ApiController::class,'EspLocation'])->name('EspLocation');
+//Route::get('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
+//Route::post('/verDatosSensor', [ApiController::class,'verDatosSensor'])->name('verDatosSensor');
+//Route::get('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
+//Route::post('/obtenerEstadoLed', [ApiController::class,'obtenerEstadoLed'])->name('obtenerEstadoLed');
+//Route::get('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
+//Route::post('/toggleLed', [ApiController::class,'switchToggleStatus'])->name('toggleLed');
+Route::get('/A9gLocation', [ApiController::class,'EspLocation'])->name('A9gLocation');
 Route::post('/LocationFromDB', [ApiController::class,'LocationFromDB'])->name('LocationFromDB');
 Route::get('/LocationFromDB', [ApiController::class,'LocationFromDB'])->name('LocationFromDB');
 //ENVIAR DATOS DEL DHT
-Route::get('/sendDHTData', [ApiController::class,'sendDHTData'])->name('sendDHTData');
-Route::post('/sendDHTData', [ApiController::class,'sendDHTData'])->name('sendDHTData');
-Route::get('/sendESPLocation', [ApiController::class,'sendESPLocation'])->name('sendESPLocation');
-Route::post('/sendESPLocation', [ApiController::class,'sendESPLocation'])->name('sendESPLocation');
+//Route::get('/sendDHTData', [ApiController::class,'sendDHTData'])->name('sendDHTData');
+//Route::post('/sendDHTData', [ApiController::class,'sendDHTData'])->name('sendDHTData');
+Route::get('/sendA9gLocation', [ApiController::class,'sendESPLocation'])->name('sendA9gLocation');
+Route::post('/sendA9gLocation', [ApiController::class,'sendESPLocation'])->name('sendA9gLocation');
