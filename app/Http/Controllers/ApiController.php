bf0477ab7a66f5bf1f335e8e0d018385b2a7cfe8
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-07 03:14:36 -0400

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -127,7 +127,7 @@ class ApiController extends Controller
     function a9glocationFromsms(Request $req){
         $sql = DB::select("SELECT geocerca from latlon_telefono ORDER BY id DESC LIMIT 1");
         $geocerca = get_object_vars($sql[0]);
-        var_dump($geocerca[0]);
+        var_dump($geocerca);
         $data = $req->all();
         $dataLoc = $data['latlon'];
         $coordinates = explode(",", $dataLoc);
