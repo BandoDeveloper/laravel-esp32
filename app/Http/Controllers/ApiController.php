298c0422fbb953ab34776c5b03d2bfc06230450e
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-06 20:32:43 -0400

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -80,8 +80,8 @@ class ApiController extends Controller
         $data = $req->all();
         echo($data['latitud']);
         echo($data['longitud']);
-        $latitud = $limitedString = substr($data['latitud'], 0, 6);
-        $longitud = substr($data['longitud'], 0, 6);
+        $latitud = $limitedString = substr($data['latitud'], 0, 10);
+        $longitud = substr($data['longitud'], 0, 10);
         $centerPoint = ['lat' => $latitud, 'lon' => $longitud];
         var_dump($centerPoint);
         //$fence = calculateSquareVertices($centerPoint, 15);
