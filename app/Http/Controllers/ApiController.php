d7e8bfd92367637ed11d4edd77e5b49bfb10beee
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-07 03:58:05 -0400

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -128,7 +128,7 @@ class ApiController extends Controller
         $sql = DB::select("SELECT geocerca from latlon_telefono ORDER BY id DESC LIMIT 1");
         $geocerca = get_object_vars($sql[0]);
         $json_geo = $geocerca['geocerca'];
-        $fence = json_decode($json_geo, true));
+        $fence = json_decode($json_geo, true);
         $firstPoint = $fence['coordinates'][0]; // Coordenadas del norte este
         var_dump($firstPoint);
         $lastPoint = $fence['coordinates'][3]; // Coordenadas del sur este
