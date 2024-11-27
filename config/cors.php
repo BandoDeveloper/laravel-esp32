f7c0a2d103917598c797ea065bc0cc888c3b5f27
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-27 17:55:28 -0400

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'], // Ajusta según tus rutas
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:8000'], // Permite tu origen
+    'allowed_origins' => ['*'], // Permite tu origen
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
