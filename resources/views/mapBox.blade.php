828cb83d7cc30866fc0940aca830bc5ff5f71eb5
BandoDeveloper <147782165+BandoDeveloper@users.noreply.github.com>
2024-11-28 00:31:03 -0400

    Update mapBox.blade.php

--- resources/views/mapBox.blade.php
+++ resources/views/mapBox.blade.php
@@ -8,7 +8,9 @@
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
     <link href="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.css" rel="stylesheet">
     <link href="https://laravel-esp32.onrender.com/js/sidebar.css" rel="stylesheet">
+    <link href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.css' rel='stylesheet' />
     <script src="https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js"></script>
+    <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.2.0/mapbox-gl-draw.js'></script>
     <style>
         body {
             margin: 0;
@@ -25,70 +27,15 @@
 </head>
 
 <body>
-    <div class="menu">
-        <ion-icon name="menu"></ion-icon>
-        <ion-icon name="close"></ion-icon>
-    </div>
-    <div class="barra-lateral">
-        <div>
-            <div class="nombre-pagina">
-                <ion-icon name="information-circle-outline" id="info"></ion-icon>
-                <span>Opciones</span>
-            </div>
-        </div>
-        <nav class="navegacion">
-            <ul>
-                <li>
-                    <a href="/">
-                        <ion-icon name="cube"></ion-icon>
-                        <span>Inicio</span>
-                    </a>
-                </li>
-                <!--<li>
-                    <a href="#">
-                        <ion-icon name="people"></ion-icon>
-                        <span>ADMIN</span>
-                    </a>
-                </li>-->
-            </ul>
-        </nav>
-        <div>
-            <div class="linea"></div>
+    <div id="map"></div>
+    <button id="guardar-geocerca" style="position: absolute; top: 10px; left: 10px; z-index: 1;">Guardar Geocerca</button>
 
-            <div class="modo-oscuro">
-                <div class="info">
-                    <ion-icon name="moon"></ion-icon>
-                    <span>Osucro</span>
-                </div>
-                <div class="switch">
-                    <div class="base">
-                        <div class="circulo">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <main>
-        <div id="map"></div>
-    </main>
-    <script>
-        mapboxgl.accessToken =
-            'pk.eyJ1IjoiZXN0ZWJhbi1tZWRyYW5vIiwiYSI6ImNsdWx3a3ZyYzE3ZW4ya3A1aWpxNG13bWgifQ.-9jmRfnuAAThDXtwiW54Jg';
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            center: [-74.5, 40], // starting position [lng, lat]
-            zoom: 20 // starting zoom
-        });
-    </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://laravel-esp32.onrender.com/js/sidebar.js"></script>
-    <script src="https://laravel-esp32.onrender.com/js/mapa.js"></script>
-
+    <!-- cambiar a  ruta de render al hacer deploy -->
+    <script src="http://127.0.0.1:8000/js/mapa.js"></script>
 </body>
 
 </html>
